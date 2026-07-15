2031893aa5c3c48db735a85889fc7befffc9a464
Muhammad Talha <muhammadtalhashahid2005@gmail.com>
2026-07-15 17:02:38 +0500

    added constants and configured the config file

---
+++ src/constant.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+# Root Directory
+
+PROJECT_ROOT = Path.cwd()
+
+# Project Directory
+
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+ARTIFICATS_DIR = PROJECT_ROOT / "artificats"
+
+MODELS_DIR = PROJECT_ROOT / "models"
+
+
+# Dataset
+
+DATASET_NAME = "student_data.csv"
+TARGET_COLUMN = "G3"
+
+
+# Defaults
+
+RANDOM_STATE = 42
+TEST_SIZE = 0.2
--- src/logger.py
+++ src/logger.py
@@ -1,15 +1,17 @@
 import os
+from pathlib import Path
 import logging
 from datetime import datetime
 
+PROJECT_ROOT = Path.cwd()
 LOG_FILE = f"{datetime.now().strftime('%y_%m_%d_%H_%M_%S')}.log"
-log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+log_path = PROJECT_ROOT / "logs"
 os.makedirs(log_path, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
+LOG_FILE_PATH = log_path / LOG_FILE
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="%(asctime)s | %(levelname)s | %(filename)s: %(lineno)d | %(message)s",
     level=logging.INFO
 )
